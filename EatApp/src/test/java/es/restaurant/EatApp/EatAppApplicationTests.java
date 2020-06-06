78b4ab410fb971d97d61b06335ff247d325877d5
evegcar <carlos.vega.garcia@ericsson.com>
2020-06-06 17:48:16 +0200

    Add initial Spring project

---
+++ EatApp/src/test/java/es/restaurant/EatApp/EatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.restaurant.EatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
