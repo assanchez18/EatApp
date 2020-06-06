2d08562f139c63236a844ea064e65b2c0b7120f2
assanchez18 <assanchez18@gmail.com>
2020-06-06 18:08:21 +0200

    Includes commensal

---
+++ src/main/java/es/restaurant/EatApp/Models/Commensal.java
@@ -0,0 +1,13 @@
+package es.restaurant.EatApp.Models;
+
+public class Commensal {
+
+	private int id;
+	public Commensal() {
+		this.id = 1;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+}
---
+++ src/test/java/es/restaurant/EatApp/Models/CommensalTest.java
@@ -0,0 +1,16 @@
+package es.restaurant.EatApp.Models;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommensalTest {
+
+	@Test
+	void getCommensalIdIs1() {
+		Commensal c = new Commensal();
+		assertTrue(c.getId() == 1, "User ID is not 1");
+	}
+	
+}
