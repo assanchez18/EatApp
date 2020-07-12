bee0b1bfa41b35436e8c87b2c19eb9dc41e8878b
assanchez18 <assanchez18@gmail.com>
2020-07-12 19:22:43 +0200

    Includes DB with schema changes

--- Documentation/fakerest1.sql
+++ Documentation/fakerest1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-07-2020 a las 14:03:20
+-- Tiempo de generación: 12-07-2020 a las 19:22:16
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.7
 
